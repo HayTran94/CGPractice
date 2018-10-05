594f6c70a7dd5bfd3db5c54cc8f9c10180fedde4
Hay Tran Van <haytranvan@Hays-MacBook-Pro.local>
2018-10-05 17:56:09 +0700

    initial commit

---
+++ CGPractice/ARCView.swift
@@ -0,0 +1,21 @@
+//
+//  ARCView.swift
+//  CGPractice
+//
+//  Created by Hay Tran Van on 10/5/18.
+//  Copyright © 2018 Hay Tran Van. All rights reserved.
+//
+
+import UIKit
+
+class ARCView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
